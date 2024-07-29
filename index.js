6025649ce33e5670bfce76fc43f369f25041abd4
tran nhat nam <tran nhat nam@LAPTOP-ESFJ9H96>
2024-07-29 20:38:06 +0700

    .

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const database = require("./config/database");
 
 const routesApiVer1 = require("./api/v1/routes/index");
 
 database.connect();
 const app = express();
+app.use(cors());
 const port = process.env.PORT;
 
 // Router api/V1
