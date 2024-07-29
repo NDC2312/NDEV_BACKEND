91f5665b58d937c5c45284fb498e8908d18c4a7a
tran nhat nam <tran nhat nam@LAPTOP-ESFJ9H96>
2024-07-29 17:35:19 +0700

    oke

---
+++ api/v1/controllers/products.js
@@ -0,0 +1,11 @@
+const Products = require("../models/task.model");
+
+// [GET] api/v1/products
+module.exports.index = async (req, res) => {
+  const products = await Products.find({
+    deleted: false,
+    status: "active",
+  });
+  console.log(products);
+  res.send("oke");
+};
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("dotenv").config();
+const database = require("./config/database");
+
+const routesApiVer1 = require("./api/v1/routes/index");
+
+database.connect();
+const app = express();
+const port = process.env.PORT;
+
+// Router api/V1
+routesApiVer1(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
